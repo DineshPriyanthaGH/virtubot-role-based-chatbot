7d483bf29baac58b6ddbf6547c3654a323246e9e
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-28 21:09:31 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import SignInPage from './pages/sign-in';
+import SignUpPage from './pages/sign-up';
+import ChatPage from './pages/chatpage';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/sign-in" element={<SignInPage />} />
-
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          {/* Add more routes as needed */}
         </Routes>
       </Router>
     </div>

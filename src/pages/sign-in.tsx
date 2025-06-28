2e3bbc2ecc4ea642a940a695356a7bedb88ec6ff
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-28 15:37:14 +0530

    sign=in page

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+// Update the path below if the actual file location or name is different
+import { SignInPage } from '../components/SignIn/SignInPage'
+export function App() {
+  return <SignInPage />
+}
+export default App

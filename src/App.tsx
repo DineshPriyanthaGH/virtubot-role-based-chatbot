6293df3b0524f66cc0760ee526bb44113f4f32b0
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-29 09:53:27 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './services/AuthProvider'

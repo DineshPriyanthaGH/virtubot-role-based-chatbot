2e111a9d4ab66aa251d8965d4cb76315f4f72fe7
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-29 08:58:13 +0530

    Up:app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import LandingPage from './pages/LandingPage';
-import SignInPage from './pages/sign-in';
-import SignUpPage from './pages/sign-up';
-import ChatPage from './pages/chatpage';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './services/AuthProvider'
+import LandingPage from './pages/LandingPage'
+import SignInPage from './pages/sign-in'
+import SignUpPage from './pages/sign-up'
+import ChatPage from './pages/chatpage'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/chatpage" element={<ChatPage />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/chatpage" element={<ChatPage />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

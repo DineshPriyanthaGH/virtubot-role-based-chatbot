eb3f64131b8b600424f935eed09a88e469aa965d
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-29 12:29:40 +0530

    update

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+// src/vite-env.d.ts
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
